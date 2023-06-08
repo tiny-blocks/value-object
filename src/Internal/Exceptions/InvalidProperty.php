9fa7aafdd3451af980a3ebb503c1b8715447b15b
gustavofreze <gustavo.freze@outlook.com>
2023-06-08 18:50:53 -0300

    feat: Updates PHP to version 8.2.

--- src/Internal/Exceptions/InvalidProperty.php
+++ src/Internal/Exceptions/InvalidProperty.php
@@ -9,6 +9,6 @@ final class InvalidProperty extends RuntimeException
     public function __construct(string $key, string $class)
     {
         $template = 'Invalid property <%s> for class <%s>.';
-        parent::__construct(sprintf($template, $key, $class));
+        parent::__construct(message: sprintf($template, $key, $class));
     }
 }
